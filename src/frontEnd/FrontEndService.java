7b7545d3ce03258e76c4f39fd976110e3d776918
esin <shubin.egor@gmail.com>
2015-05-15 23:14:37 +0300

    wut? public interface methods? O_o

--- src/frontEnd/FrontEndService.java
+++ src/frontEnd/FrontEndService.java
@@ -4,15 +4,15 @@ package frontEnd;
  * @author e.shubin
  */
 public interface FrontEndService {
-    public void register(String name, String password);
+    void register(String name, String password);
 
-    public boolean isRegistered(String name);
+    boolean isRegistered(String name);
 
-    public String authenticate(String name, String password);
+    String authenticate(String name, String password);
 
-    public boolean isAuthenticated(String sessionId);
+    boolean isAuthenticated(String sessionId);
 
-    public int getScore(String sessionId);
+    int getScore(String sessionId);
 
-    public void updateScore(String sessionId, int delta);
+    void updateScore(String sessionId, int delta);
 }
